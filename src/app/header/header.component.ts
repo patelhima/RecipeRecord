5fccf54ee284ffe5927667f19b444a800c85f514
hpatel <hpatel@navaera.com>
2020-08-31 15:39:24 +0530

    section-12
    section-12: reverted bootstrap version to 3
    committed changes for basic app working with route

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,14 +7,14 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  @Output() featureSelected = new EventEmitter<string>();
+  /*@Output() featureSelected = new EventEmitter<string>();*/
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onSelect(feature: string){
+  /*onSelect(feature: string){
     this.featureSelected.emit(feature);
-  }
+  }*/
 }
